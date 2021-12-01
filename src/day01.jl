90455917bd93865fe604fabc9139b14e82738d7b
Lars Franke <frcl@mailbox.org>
2021-12-01 08:32:23 +0100

    Add day 1 solution

--- src/day01.jl
+++ src/day01.jl
@@ -1,3 +1,23 @@
-function day01_part1(input::Vector{String})
-    print("Hello World! Read $(length(input)) lines.")
+function day01_part1(input::Vector{<:AbstractString})
+    depth_values = parse_input_d1(input)
+    return count_incs(depth_values)
+end
+
+
+function day01_part2(input::Vector{<:AbstractString})
+    depth_values = parse_input_d1(input)
+    n = length(depth_values)
+    return count_incs(depth_values[1:n-2] + depth_values[2:n-1] + depth_values[3:n])
+end
+
+
+function parse_input_d1(input::Vector{<:AbstractString})
+    return [parse(Int32, i) for i = input]
+end
+
+
+function count_incs(vec::Vector{Int32})
+    n = length(vec)
+    inc = vec[2:n] - vec[1:n-1]
+    return sum([1 for i = inc if i > 0])
 end
--- test/test_day01.jl
+++ test/test_day01.jl
@@ -1,8 +1,9 @@
 @testset "Day 1" begin
     @testset "Part 1" begin
-        @test 1 == 1
+        @test aoc2021.day01_part1(split("199\n200\n208\n210\n200\n207\n240\n269\n260\n263")) == 7
     end
 
     @testset "Part 2" begin
+        @test aoc2021.day01_part2(split("199\n200\n208\n210\n200\n207\n240\n269\n260\n263")) == 5
     end
 end
